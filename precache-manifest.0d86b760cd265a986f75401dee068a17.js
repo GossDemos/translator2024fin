5008bc1177be1f540a504d8a429f5fd704478f6b
AnTs <34278850+AndrewTsuranov@users.noreply.github.com>
2024-07-08 12:42:12 +0300

    Add files via upload

---
+++ precache-manifest.0d86b760cd265a986f75401dee068a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506a4ba10e05c4678dbcc69ffa38f76c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eefedcf1f3e6a6d69e6b",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e40ddf2b9b671016ed71",
+    "url": "/static/js/2.ef63edac.chunk.js"
+  },
+  {
+    "revision": "eefedcf1f3e6a6d69e6b",
+    "url": "/static/js/main.f3255e6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
